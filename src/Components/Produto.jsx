e940f1ae3fec8c4727daed8c4b91edb81deb6cfc
JoãoB <joaob.dev@gmail.com>
2023-11-12 23:53:39 -0300

    commit final

--- src/Components/Produto.jsx
+++ src/Components/Produto.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./Produto.module.css";
 import { useParams } from "react-router-dom";
-import Produtos from "./Produtos";
 import Head from "./Head";
 
 const Produto = () => {
@@ -27,22 +26,24 @@ const Produto = () => {
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <div className="loading"></div>;
   if (error) return <p>{error}</p>;
   if (produto == null) return null;
   return (
     <section className={styles.Produto + " animeLeft"}>
-      <Head 
-      title={`Ranek | ${produto.nome}`}
-      description={`Ranek | Esse é o nome do produto: ${produto.nome}`} 
+      <Head
+        title={`Ranek | ${produto.nome}`}
+        description={`Ranek | Esse é o nome do produto: ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => ( 
-      <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
       <div>
-          <h1>{produto.nome}</h1>
-          <span className={styles.preco}>R$ {produto.preco}</span>
-          <p className={styles.descricao}>{produto.descricao}</p>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
       </div>
     </section>
   );
